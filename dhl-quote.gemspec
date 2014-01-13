2ed87419f46c42edd566019e5ef2bc76728d30bb
Muamer Ribica <muamer.ribica@gmail.com>
2014-01-13 10:12:47 +0100

    debug response

--- dhl-quote.gemspec
+++ dhl-quote.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "dhl-quote"
-  s.version     = "0.0.2"
+  s.version     = "0.1.3"
   s.date        = "2013-12-15"
   s.summary     = "dhl shipping quote"
   s.description = "dhl shipping quote"
--- lib/dhl-quote/response.rb
+++ lib/dhl-quote/response.rb
@@ -6,6 +6,9 @@ module Shipping
       attr_accessor :shipping_charge
 
       def initialize(response)
+        puts "---------"
+        puts response.body
+        puts "---------"
         parsed_response = Nokogiri.XML(response.body)
         @shipping_charge = parsed_response.xpath("//ShippingCharge").text
 
